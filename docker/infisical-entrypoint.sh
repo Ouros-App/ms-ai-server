c0445133c32619dfbb85bcf08578fd32833bf452
nicolas vlad <nicolas25vlad@proton.me>
2026-09-06 01:23:06 -0300

    feat: integrar secrets do Infisical no runtime

---
+++ docker/infisical-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+: "${INFISICAL_TOKEN:?INFISICAL_TOKEN is required}"
+: "${INFISICAL_PROJECT_ID:?INFISICAL_PROJECT_ID is required}"
+
+exec infisical run \
+  --projectId="$INFISICAL_PROJECT_ID" \
+  --env="${INFISICAL_ENV:-prod}" \
+  --path="${INFISICAL_PATH:-/ms-ai-server}" \
+  -- "$@"
